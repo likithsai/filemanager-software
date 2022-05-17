1790a10945e493aca8f2356851d1c26de2249daa
likithsai <likithsai13@gmail.com>
2022-05-17 07:13:25 +0530

    Minor UI change

--- client/src/App.js
+++ client/src/App.js
@@ -1,119 +1,139 @@
 import React from 'react';
 import Header from './component/Header';
 import Footer from './component/Footer';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link
+} from 'react-router-dom';
 
 const App = () => {
     return (
-        <>
+        <div className="app">
             <Header title="Media Vault" />
-
+            <div class="h-100 p-5 text-white bg-dark">
+                <div className="container-md px-md-3 shadow-sm">
+                    <h2>Change the background</h2>
+                    <p>Swap the background-color utility and add a `.text-*` color utility to mix up the jumbotron look. Then, mix and match with additional component themes and more.</p>
+                </div>
+            </div>
             <main>
                 <div class="album py-5 bg-light">
-                    <div class="container">
+                    <div class="container px-4">
                         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
                             <div class="col">
-                                <div class="card shadow-sm">
-                                    <img src="https://picsum.photos/200" width="100%" height="250" />
+                                <div class="card">
+                                    <img src="https://via.placeholder.com/250x250" width="100%" height="250" />
                                     <div class="card-body">
                                         <p class="card-text fw-bold">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                                         <div class="d-flex justify-content-between align-items-center">
-                                            <small class="text-muted">9 MB</small>
+                                            <small class="text-dark">9 MB</small>
+                                            <small class="text-muted">Thu 15, 2019</small>
                                         </div>
                                     </div>
                                 </div>
                             </div>
 
                             <div class="col">
-                                <div class="card shadow-sm">
+                                <div class="card">
                                     <img src="https://picsum.photos/200" width="100%" height="250" />
                                     <div class="card-body">
                                         <p class="card-text fw-bold">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                                         <div class="d-flex justify-content-between align-items-center">
-                                            <small class="text-muted">9 MB</small>
+                                            <small class="text-dark">9 MB</small>
+                                            <small class="text-muted">Thu 15, 2019</small>
                                         </div>
                                     </div>
                                 </div>
                             </div>
 
                             <div class="col">
-                                <div class="card shadow-sm">
+                                <div class="card">
                                     <img src="https://picsum.photos/200" width="100%" height="250" />
                                     <div class="card-body">
                                         <p class="card-text fw-bold">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                                         <div class="d-flex justify-content-between align-items-center">
-                                            <small class="text-muted">9 MB</small>
+                                            <small class="text-dark">9 MB</small>
+                                            <small class="text-muted">Thu 15, 2019</small>
                                         </div>
                                     </div>
                                 </div>
                             </div>
 
                             <div class="col">
-                                <div class="card shadow-sm">
+                                <div class="card">
                                     <img src="https://picsum.photos/200" width="100%" height="250" />
                                     <div class="card-body">
                                         <p class="card-text fw-bold">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                                         <div class="d-flex justify-content-between align-items-center">
-                                            <small class="text-muted">9 MB</small>
+                                            <small class="text-dark">9 MB</small>
+                                            <small class="text-muted">Thu 15, 2019</small>
                                         </div>
                                     </div>
                                 </div>
                             </div>
 
                             <div class="col">
-                                <div class="card shadow-sm">
+                                <div class="card">
                                     <img src="https://picsum.photos/200" width="100%" height="250" />
                                     <div class="card-body">
                                         <p class="card-text fw-bold">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                                         <div class="d-flex justify-content-between align-items-center">
-                                            <small class="text-muted">9 MB</small>
+                                            <small class="text-dark">9 MB</small>
+                                            <small class="text-muted">Thu 15, 2019</small>
                                         </div>
                                     </div>
                                 </div>
                             </div>
 
                             <div class="col">
-                                <div class="card shadow-sm">
+                                <div class="card">
                                     <img src="https://picsum.photos/200" width="100%" height="250" />
                                     <div class="card-body">
                                         <p class="card-text fw-bold">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                                         <div class="d-flex justify-content-between align-items-center">
-                                            <small class="text-muted">9 MB</small>
+                                            <small class="text-dark">9 MB</small>
+                                            <small class="text-muted">Thu 15, 2019</small>
                                         </div>
                                     </div>
                                 </div>
                             </div>
 
                             <div class="col">
-                                <div class="card shadow-sm">
+                                <div class="card">
                                     <img src="https://picsum.photos/200" width="100%" height="250" />
                                     <div class="card-body">
                                         <p class="card-text fw-bold">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                                         <div class="d-flex justify-content-between align-items-center">
-                                            <small class="text-muted">9 MB</small>
+                                            <small class="text-dark">9 MB</small>
+                                            <small class="text-muted">Thu 15, 2019</small>
                                         </div>
                                     </div>
                                 </div>
                             </div>
 
                             <div class="col">
-                                <div class="card shadow-sm">
+                                <div class="card">
                                     <img src="https://picsum.photos/200" width="100%" height="250" />
                                     <div class="card-body">
                                         <p class="card-text fw-bold">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                                         <div class="d-flex justify-content-between align-items-center">
-                                            <small class="text-muted">9 MB</small>
+                                            <small class="text-dark">9 MB</small>
+                                            <small class="text-muted">Thu 15, 2019</small>
                                         </div>
                                     </div>
                                 </div>
                             </div>
 
                             <div class="col">
-                                <div class="card shadow-sm">
+                                <div class="card">
                                     <img src="https://picsum.photos/200" width="100%" height="250" />
                                     <div class="card-body">
                                         <p class="card-text fw-bold">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                                         <div class="d-flex justify-content-between align-items-center">
-                                            <small class="text-muted">9 MB</small>
+                                            <small class="text-dark">9 MB</small>
+                                            <small class="text-muted">Thu 15, 2019</small>
                                         </div>
                                     </div>
                                 </div>
@@ -123,8 +143,8 @@ const App = () => {
                 </div>
             </main>
 
-            <Footer title="© likith sai"/>
-        </>
+            {/* <Footer title="© likith sai"/> */}
+        </div>
     );
 }
 
--- client/src/component/Header.js
+++ client/src/component/Header.js
@@ -6,54 +6,52 @@ const Header = (props) => {
         <header className="shadow-sm sticky-top bg-dark">
             <div className="collapse" id="navbarHeader">
                 <div className="container">
-                <div className="row">
-                    <div className="col-sm-12 col-md-12 py-4">
-                    {/* <p className="text-muted mb-2 px-0">Add some information about the album below, the author, or any other background context. Make it a few sentences long so folks can pick up some informative tidbits. Then, link them off to some social networking sites or contact information.</p> */}
-                    
-                    <ul class="list-group">
-                        <li class="list-group-item text-white bg-dark px-0">
-                            <div class="d-flex align-items-center justify-content-between">
-                                <a href="/images" class="text-white text-decoration-none">
-                                    <i class="bi bi-images me-3"></i>
-                                    <span>Images</span>
-                                </a>
-                                <div class="d-flex align-item-center badge bg-secondary">
-                                    <span class="me-2">12</span>
-                                    <i class="bi bi-arrow-right-circle-fill text-white"></i>
-                                </div>
-                            </div>
-                        </li>
-                        <li class="list-group-item text-white bg-dark px-0">
-                            <div class="d-flex align-items-center justify-content-between">
-                                <a href="/videos" class="text-white text-decoration-none">
-                                    <i class="bi bi-camera-reels me-3"></i>
-                                    <span>Videos</span>
-                                </a>
-                                <div class="d-flex align-item-center badge bg-secondary">
-                                    <span class="me-2">463</span>
-                                    <i class="bi bi-arrow-right-circle-fill text-white"></i>
-                                </div>
-                            </div>
-                        </li>
-                        <li class="list-group-item text-white bg-dark px-0">
-                            <div class="d-flex align-items-center justify-content-between">
-                                <a href="/pdfs" class="text-white text-decoration-none">
-                                    <i class="bi bi-file-earmark-pdf me-3"></i>
-                                    <span>PDF</span>
-                                </a>
-                                <div class="d-flex align-item-center badge bg-secondary">
-                                    <span class="me-2">7</span>
-                                    <i class="bi bi-arrow-right-circle-fill text-white"></i>
-                                </div>
-                            </div>
-                        </li>
-                    </ul>
+                    <div className="row px-3">
+                        <div className="col-sm-12 col-md-12 py-4">    
+                            <ul class="list-group">
+                                <li class="list-group-item text-white bg-dark px-0">
+                                    <div class="d-flex align-items-center justify-content-between">
+                                        <a href="/images" class="text-white text-decoration-none">
+                                            <i class="bi bi-images me-3"></i>
+                                            <span>Images</span>
+                                        </a>
+                                        <div class="d-flex align-item-center badge bg-secondary">
+                                            <span class="me-2">12</span>
+                                            <i class="bi bi-arrow-right-circle-fill text-white"></i>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li class="list-group-item text-white bg-dark px-0">
+                                    <div class="d-flex align-items-center justify-content-between">
+                                        <a href="/videos" class="text-white text-decoration-none">
+                                            <i class="bi bi-camera-reels me-3"></i>
+                                            <span>Videos</span>
+                                        </a>
+                                        <div class="d-flex align-item-center badge bg-secondary">
+                                            <span class="me-2">463</span>
+                                            <i class="bi bi-arrow-right-circle-fill text-white"></i>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li class="list-group-item text-white bg-dark px-0">
+                                    <div class="d-flex align-items-center justify-content-between">
+                                        <a href="/pdfs" class="text-white text-decoration-none">
+                                            <i class="bi bi-file-earmark-pdf me-3"></i>
+                                            <span>PDF</span>
+                                        </a>
+                                        <div class="d-flex align-item-center badge bg-secondary">
+                                            <span class="me-2">7</span>
+                                            <i class="bi bi-arrow-right-circle-fill text-white"></i>
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
-                </div>
                 </div>
             </div>
             <div className="navbar navbar-dark bg-dark shadow-sm">
-                <div className="container">
+                <div className="container px-4">
                 <a href="#" className="navbar-brand d-flex align-items-center">
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" aria-hidden="true" class="me-2" viewBox="0 0 24 24"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"></path><circle cx="12" cy="13" r="4"></circle></svg>
                     <strong>{props.title}</strong>
